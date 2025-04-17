51c0e03bafe5cf11f40312e3c769f1cc42ab9295
Arthurrmande <arthur.mande@etu.unilim.fr>
2025-04-17 20:18:37 +0200

    #

--- SAE203_iteration_surprise/app/component/FilmMisenavant/script.js
+++ SAE203_iteration_surprise/app/component/FilmMisenavant/script.js
@@ -8,31 +8,27 @@ async function loadTemplate() {
 FilmMisenavant.format = async function (movies) {
   if (!movies.length) return "";
 
-  let template = await loadTemplate(); 
+  let template = await loadTemplate();
   let card = "";
-
-  for (let index = 0; index < movies.length; index++) {
-    const movie = movies[index];
+  for (let movie of movies) {
     const image = movie.image ?? "placeholder.jpg";
     const name = movie.name ?? "Sans titre";
-    const age = movie.min_age ?? "N.C";
-    const year = movie.year ?? "????";
-    const category = movie.category_name ?? "Inconnu";
     const description = movie.description ?? "Aucune description disponible.";
 
     card += `
-      <div class="mea__card" onclick="C.handlerDetail(${movie.id})" style="--i:${index + 1}">
-        <div class="mea__img">
-          <img class="mea__image" src="../server/images/${image}" alt="${name}" />
-          <div class="mea__overlay">
-            <h3 class="mea__name">${name}</h3>
-            <p class="mea__desc">${description}</p>
-          </div>
+      <div class="mea__slide" onclick="C.handlerDetail(${movie.id})">
+        <img class="mea__image" src="../server/images/${image}" alt="${name}" />
+        <div class="mea__overlay">
+          <h3 class="mea__name">${name}</h3>
+          <p class="mea__desc">${description}</p>
         </div>
-      </div>`;
+      </div>
+    `;
   }
 
   return template.replace("{{card}}", card);
 };
+
+
 
 export { FilmMisenavant };
