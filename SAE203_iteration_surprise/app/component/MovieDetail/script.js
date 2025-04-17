3aa9d9ef59f3da7b24d6ce56d4df5810f6507850
Arthurrmande <arthur.mande@etu.unilim.fr>
2025-04-17 16:29:49 +0200

    #

--- SAE203_iteration_surprise/app/component/MovieDetail/script.js
+++ SAE203_iteration_surprise/app/component/MovieDetail/script.js
@@ -18,6 +18,9 @@ MovieDetail.format = function (movie) {
   detail = detail.replace("{{categorie}}", movie.category_name);
   detail = detail.replace("{{age}}", movie.min_age);
   detail = detail.replace("{{note}}", movie.note);
+
+  detail = detail.replace("{{handlerfavori}}", `event.stopPropagation(); C.handlerfavori(${movie.id})`);
+  detail = detail.replace("{{handlerdelfav}}", `event.stopPropagation(); C.handlerdelfav(${movie.id})`);
   console.log(movie.note);
 
   return detail;
